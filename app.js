0c4d67d79ba3f4ef39394c225c764f966b152ac3
Oğuzhan <akcahanoguz@gmail.com>
2021-10-13 15:37:36 +0300

    Ejs,partials,views added.

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const ejs = require('ejs');
+const path = require('path');
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.render('index');
+})
+
+app.get('/about', (req, res) => {
+  res.render('about');
+})
+
+app.get('/add', (req, res) => {
+  res.render('add');
+})
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});

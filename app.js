cabf725246bf8ebed5a7a85e3da143141bd0169e
Oğuzhan <akcahanoguz@gmail.com>
2021-10-14 23:34:29 +0300

    Mongoose and some details are added.

--- app.js
+++ app.js
@@ -1,24 +1,35 @@
-const express = require('express');
-const ejs = require('ejs');
-const path = require('path');
+const express = require("express");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ejs = require("ejs");
+const path = require("path");
 const app = express();
+const Photo = require("./models/Photo");
 
 app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+mongoose.connect('mongodb://localhost/blog-test-db');
 
-app.get('/', (req, res) => {
-  res.render('index');
-})
-
-app.get('/about', (req, res) => {
-  res.render('about');
-})
-
-app.get('/add', (req, res) => {
-  res.render('add');
-})
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+  res.render("index", {
+    photos,
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+app.post("/photos", async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect("/");
+});
 
 const port = 3000;
 app.listen(port, () => {

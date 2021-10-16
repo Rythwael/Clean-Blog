ae019e11264d4404ac44d31fffddb1ff0fceff8b
Oğuzhan <akcahanoguz@gmail.com>
2021-10-16 17:58:43 +0300

    Controllers,update,delete buttons added.

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const ejs = require("ejs");
-const path = require("path");
+const fileUpload = require("express-fileupload");
+const methodOverride = require('method-override');
 const app = express();
-const Post = require("./models/Posts");
+const pageController = require('./controllers/pageController');
+const postController = require('./controllers/postController');
+
 
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
 
 mongoose.connect('mongodb://localhost/blog-test-db');
 
-app.get("/", async (req, res) => {
-  const posts = await Post.find({});
-  res.render("index", {
-    posts
-  });
-});
-app.get("/posts/:id", async (req, res) => {
-  const post = await Post.findById(req.params.id);
-  res.render("post",{
-    post
-  });
-});
-app.get("/about", (req, res) => {
-  res.render("about");
-});
-app.get("/add", (req, res) => {
-  res.render("add");
-});
-app.post("/posts", async (req, res) => {
-  await Post.create(req.body);
-  res.redirect("/");
-});
+app.get("/posts/edit/:id", pageController.getEditPage);
+app.get("/about", pageController.getAboutPage);
+app.get("/add", pageController.getAddPage);
+
+app.get("/", postController.getAllPosts);
+app.get("/posts/:id", postController.getPost);
+app.post("/posts", postController.createPost);
+app.put("/posts/:id", postController.updatePost);
+app.delete("/posts/:id", postController.deletePost);
 
 const port = 3000;
 app.listen(port, () => {
